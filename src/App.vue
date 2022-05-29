ec05b265a89f6dc63d9a576f486ad56c198c6d83
平安喜樂 <10823224+xsfighting@user.noreply.gitee.com>
2022-05-29 15:34:20 +0800

    提交信息

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div id="app" class="wrapper">
     <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
+<script>
+import MainTabBar from '@/components/content/mainTabbar/MainTabBar'
+export default {
+  name: 'app',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
 <style>
-
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "assets/css/base.css";
 </style>
